bf2782f07c235cd744bf152778a03ac5388c2546
miku <nunon4msmrn@icloud.com>
2023-12-08 20:11:53 +0900

    自機操作

--- game-ver1.py
+++ game-ver1.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 
 SCREEN_WIDTH = 500
-SCREEN_HEIGHT = 300
+SCREEN_HEIGHT = 600
 
 player_x = SCREEN_WIDTH // 2 
 player_y = SCREEN_HEIGHT // 2
@@ -13,22 +13,48 @@ text = None
 
 def updata():
   global running
+  global player_x
+  global player_y
 
   for event in pygame.event.get():
     if event.type == QUIT:
       running = False
 
+  # playerの操作
+  pressed_keys = pygame.key.get_pressed()
+
+  if pressed_keys[pygame.K_UP]:
+    player_y = player_y - 3
+    if player_y < 10:
+      player_y = 10
+
+  if pressed_keys[pygame.K_DOWN]:
+    player_y = player_y + 3
+    if player_y > SCREEN_HEIGHT - 10:
+      player_y = SCREEN_HEIGHT - 10
+
+  if pressed_keys[pygame.K_LEFT]:
+    player_x = player_x - 3
+    if player_x < 10:
+      player_x = 10
+
+  if pressed_keys[pygame.K_RIGHT]:
+    player_x = player_x + 3
+    if player_x > SCREEN_WIDTH - 10:
+      player_x = SCREEN_WIDTH - 10
+
 
 def draw(screen):
   screen.fill((255,255,255))
 
+  # テキストの描写
   screen_size = screen.get_size()
   text_size = text.get_rect()
   text_size.center = (screen_size[0] // 2, screen_size[1] //5)
-
-  pygame.draw.circle(screen, (0,0,0), (player_x,player_y), 10)
-
   screen.blit(text,text_size)
+
+  # playerの描写
+  pygame.draw.circle(screen, (0,0,0), (player_x,player_y), 10)
 
   pygame.display.update()
 
@@ -41,10 +67,13 @@ def run():
   pygame.display.set_caption("Test")
   font = pygame.font.Font(None,40)
   text = font.render('Hello python',True,(000,000,000))
+  clock = pygame.time.Clock()
 
   while running:
     updata()
     draw(screen)
+
+    clock.tick(100)
     
   pygame.quit()
 
